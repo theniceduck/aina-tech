8ca436f2d4b798f8d6a4e2268c553c92edf14f4f
theniceduck <niceduck77@gmail.com>
2025-03-27 23:19:54 +0800

    some change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aina.tech/', // Set this to your GitHub repo name
   plugins: [react()],
+  base: '/aina.tech/', // Set this to your GitHub repo name
 })
